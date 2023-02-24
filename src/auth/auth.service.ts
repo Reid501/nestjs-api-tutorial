cb95b088b611f356870ac9eaedf304251e1dcfb1
reid501 <alexatrs@hotmail.co.uk>
2023-02-24 16:18:28 +0000

    sign in service added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -32,7 +32,25 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { msg: 'I am signed in' };
+  async signin(dto: AuthDto) {
+    // Find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    // If no user throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    // Compatr password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    // If password incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    delete user.hash;
+
+    return user;
   }
 }
