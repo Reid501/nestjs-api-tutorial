ec1d4cda9a6eb17aa09ab184a962421234e8ba76
reid501 <alexatrs@hotmail.co.uk>
2023-02-27 15:04:20 +0000

    get user decorator added

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,21 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStratergy extends PassportStrategy(Strategy) {
-  constructor(private config: ConfigService) {
+  constructor(private config: ConfigService, private prisma: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
   }
 
-  //   async validate(username: string, password: string): Promise<any> {
-  //     const user = await this.authService.validateUser(username, password);
-  //     if (!user) {
-  //       throw new UnauthorizedException();
-  //     }
-  //     return user;
-  //   }
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+    delete user.hash;
+    return user;
+  }
 }
